4018b62f63ae7cf964abd4372953e3781241d659
lewmeta <dev.lewismeta@gmail.com>
2026-04-14 16:28:26 +0300

    03: trcp setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
-import { Button } from "@/components/ui/button";
-import prisma from "@/lib/db";
-import Image from "next/image";
+// import { useTRPC } from "@/trpc/trpc";
+import { dehydrate, HydrationBoundary, useQuery } from "@tanstack/react-query";
+import { Client } from "./client";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { Suspense } from "react";
 
-export default async function Home() {
+// import { caller } from "@/trpc/server";
 
-  const users = await prisma.user.findFirst();
-  console.log(users);
+const Page = async () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+  // const users = await caller.getUsers();
+
   return (
-    <div>
-      <Button variant={'ghost'}>{users?.name}</Button>
+    <div className="flex items-center justify-center min-h-screen">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Client />
+        </Suspense>
+      </HydrationBoundary>
     </div>
   );
 }
+
+export default Page;
