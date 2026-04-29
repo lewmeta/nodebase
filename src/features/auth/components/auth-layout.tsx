a31e5f00545df0b20f6439e22fffad1e6e6bdae3
lewmeta <dev.lewismeta@gmail.com>
2026-04-29 13:42:38 +0300

    06: background jobs

--- src/features/auth/components/auth-layout.tsx
+++ src/features/auth/components/auth-layout.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export const AuthLayout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="flex flex-col items-center justify-center gap-6 p-6 bg-muted min-h-svh md:p-10">
+        <div className="flex flex-col items-center justify-center gap-6 p-6 bg-muted min-h-svh md:p-10 dark">
             <div className="flex flex-col w-full max-w-sm gap-6">
                 <Link
                     href="/"
